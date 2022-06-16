8cc1fba31091f59b00fcd574ae76a97b2a615247
Hal@shurabaP <harunyann@hotmail.com>
2022-06-16 23:11:31 +0900

    Add WORLDsynthesizer bindings

---
+++ wasm/src/world/SynthesizeWaveformWithWORLDBindings.cpp
@@ -0,0 +1,14 @@
+// Copyright 2022 Hal@shurabaP.  All rights reserved.
+// Use of this source code is governed by a MIT style
+// license that can be found in the LICENSE file.
+#include "world/SynthesizeWaveformWithWORLD.hpp"
+#include <emscripten/bind.h>
+
+using namespace emscripten;
+
+EMSCRIPTEN_BINDINGS(SynthesizeWaveformWithWORLDBindings) {
+    class_<uzume::vocoder::world::SynthesizeWaveformWithWORLD, base<uzume::vocoder::SynthesizeWaveform> >("SynthesizeWaveformWithWORLD")
+            .smart_ptr<std::shared_ptr<uzume::vocoder::world::SynthesizeWaveformWithWORLD>>("shared_ptr<uzume::vocoder::world::SynthesizeWaveformWithWORLD>")
+            .constructor(&std::make_shared<uzume::vocoder::world::SynthesizeWaveformWithWORLD>)
+            .function("synthesis", &uzume::vocoder::world::SynthesizeWaveformWithWORLD::operator(), allow_raw_pointers());
+}
