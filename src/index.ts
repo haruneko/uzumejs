8c1b74cff7403068ba2c84212a877f5c391ed899
Hal@shurabaP <harunyann@hotmail.com>
2022-06-26 22:38:05 +0900

    aggregate export classes

--- src/index.ts
+++ src/index.ts
@@ -9,17 +9,4 @@ const uzumejs: (...args: any[]) => Promise<UzumeJs> = (args) => {
 }
 
 export default uzumejs;
-
-export type ArraySpectrogramAggregator = Wasm.ArraySpectrogramAggregator;
-export type Contour = Wasm.Contour;
-export type EstimateF0 = Wasm.EstimateF0;
-export type EstimateF0WithDIO = Wasm.EstimateF0WithDIO;
-export type LinearTimeAxisMap = Wasm.LinearTimeAxisMap;
-export type Spectrogram = Wasm.Spectrogram;
-export type SpectrogramVector = Wasm.SpectrogramVector;
-export type Spectrum = Wasm.Spectrum;
-export type StretchedPartialSpectrogram = Wasm.StretchedPartialSpectrogram;
-export type SynthesizeWaveform = Wasm.SynthesizeWaveform;
-export type SynthesizeWaveformWithWORLD = Wasm.SynthesizeWaveformWithWORLD;
-export type Waveform = Wasm.Waveform;
-export type WaveformSpectrogram = Wasm.WaveformSpectrogram;
+export * from "../resources/uzumewasm"
